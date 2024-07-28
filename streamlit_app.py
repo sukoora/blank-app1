7754410d01a4f8665a8feafdfedcb77b33db4e88
Huan Huang <sukoora@qq.com>
2024-07-29 07:21:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,27 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+placeholder = st.empty()
+
+# Replace the placeholder with some text:
+placeholder.text("Hello")
+
+# Replace the text with a chart:
+placeholder.line_chart({"data": [1, 5, 2, 6]})
+
+# Replace the chart with several elements:
+with placeholder.container():
+    st.write("This is one element")
+    st.write("This is another")
+
+# Clear all those elements:
+placeholder.empty()
+
+import time
+
+with st.empty():
+    for seconds in range(60):
+        st.button(f"Refresh")
+        time.sleep(1)
+    st.write("✔️ 1 minute over!")
